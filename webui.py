f9dc915a5c2fc3046ae7961029504ac856934d2c
jackgo <jackgo2080@gmail.com>
2023-05-13 10:46:13 +0800

    port 3004

--- webui.py
+++ webui.py
@@ -388,7 +388,7 @@ with gr.Blocks(css=block_css) as demo:
 (demo
  .queue(concurrency_count=3)
  .launch(server_name='0.0.0.0',
-         server_port=7860,
+         server_port=3004,
          show_api=False,
          share=False,
          inbrowser=False))
